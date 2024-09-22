ef4cc11f5ab4acc52c0b3358b3a02d75d5a4d267
Paulo Seares <athaneduc@gmail.com>
2024-09-22 07:54:37 -0700

    Chapter 9

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,13 @@
 export default function Page() {
     return <p>Dashboard Page</p>;
   }
+
+  
+  
+// import CardWrapper from '@/app/ui/dashboard/cards';
+
+// import {
+//   RevenueChartSkeleton,
+//   LatestInvoicesSkeleton,
+//   CardsSkeleton,
+// } from '@/app/ui/skeletons';
